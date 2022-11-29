8cac5efb59ffa2b69c63089b205df12c043092a9
The-Genius001 <jcr4576@gmail.com>
2022-11-29 16:29:09 +0000

    hello

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ function App() {
   return (
     <div className="{theGenius}">
       <header className="App-header">
+        <title>theGenius</title>
         <h1>
-          <a className="small">GITHUB</a>
-          <a className="small">GAMES</a>
-          <a className="small">HOME</a>
-          <a className="small">PROJECTS</a>
-          <a className="small">WEBAPPS</a>
+          <a className="small" href="">GITHUB</a>
+          <a className="small" href="">GAMES</a>
+          <a className="small" href="">HOME</a>
+          <a className="small" href="">PROJECTS</a>
+          <a className="small" href="">WEBAPPS</a>
         </h1>
-   
+        <h2 className="t">theGenius</h2>
         <p>
           
         </p>
