e12ec6ff673f4bd0755ba0d00c55af060ff6dde1
The-Genius001 <jcr4576@gmail.com>
2022-12-06 22:51:55 +0000

    hello gain( worked on adding more function to the site)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-
+var page;
 function App() {
-  
+  if (page="home"||page==""){
   
   return (
     <div className="{theGenius}">
@@ -11,11 +11,11 @@ function App() {
           
         </style>
         <h1>
-          <a className="small" href="">GITHUB</a>
-          <a className="small" href="">GAMES</a>
-          <a className="small" href="">HOME</a>
-          <a className="small" href="">PROJECTS</a>
-          <a className="small" href="">WEBAPPS</a>
+          <a className="small" >GITHUB</a>
+          <a className="small" onClick={ud()}>GAMES</a>
+          <a className="small" >HOME</a>
+          <a className="small" >PROJECTS</a>
+          <a className="small" >WEBAPPS</a>
         </h1>
         
         <h2 className="t">theGenius</h2>
@@ -40,6 +40,18 @@ function App() {
       </header>
     </div>
   );
+}else if(page="ud"){
+return(<h1> this is a page about unknown deapths</h1>);
+}else{
+  return(<div>
+    <h2> ERROR page does not exist i think </h2>
+  <a className='small' onClick={page="home"} >GO HOME</a>
+  </div>
+  )
+}
+}
+function ud(){
+page="ud";
 }
 
 export default App;
